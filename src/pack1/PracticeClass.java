83f0e87a26b928a500a98ae8095d2a1f71b6d68a
SIDDHANT <SIDDHANT@DESKTOP-A55II15>
2022-08-07 15:20:03 +0530

    Practice class- second time pushing the code on github

---
+++ src/pack1/PracticeClass.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class PracticeClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("Bye");
+	}
+
+}
